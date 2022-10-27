f28d6a4da6aefe12ee9e8ee0cdc6684f077314d6
TikiCala <104035384+TikiCala@users.noreply.github.com>
2022-10-27 00:50:13 -0300

    Add files via upload

--- stock_prendas/listar.php
+++ stock_prendas/listar.php
@@ -12,9 +12,7 @@
     <h1>Stock prendas de ropa</h1>
     <button type="submit"><a href="./index.html">Inicio</a></button>
     <button type="submit"><a href="./listar.php">Listar ropa</a></button>
-    <button type="submit"><a href="./buzos.php">Buzos</a></button>
-    <button type="submit"><a href="./nike.php">Nike</a></button>
-    <button type="submit"><a href="./preciomayora500.php">Precio mayor a $500</a></button>
+    <button type="submit"><a href="./agregar.html">Agregar prenda</a></button>
     <h2>Lista</h2>
     <p>la siguiente lista muestra los datos de la ropa actualmente en stock</p>
     <table border="1">
@@ -24,6 +22,9 @@
             <th>MARCA</th>
             <th>TALLE</th>
             <th>PRECIO</th>
+            <th>IMAGEN</th>
+            <th>EDITAR</th>
+            <th>BORRAR</th>
         </tr>
 
         <?php
@@ -42,6 +43,10 @@
                 <td><?php echo $fila['marca']; ?></td>
                 <td><?php echo $fila['talle']; ?></td>
                 <td><?php echo $fila['precio']; ?></td>
+                <td><img src="data:image/jpeg;base64, <?php echo base64_encode($fila['imagen'])?>" alt="" width="100px" height="100px"></td>
+                <td><a href="./modificar.php?id=<?php echo $fila['id']; ?>">Editar</a></td>
+                <td><a href="./borrar.php?id=<?php echo $fila['id']; ?>">Borrar</a></td>
+                
             </tr>
         <?php }
         ?>
